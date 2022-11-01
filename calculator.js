9f82f9d604cb62f273d1bae59ad8a8708d98842b
scriptax <moradii97@outlook.com>
2022-11-02 00:24:00 +0330

    Calculator is operational

--- calculator.js
+++ calculator.js
@@ -1,92 +1,75 @@
 var expCalc = "";
 var answer = "";
+var reset = false;
 
 const checker = {
-    lastIsMulDiv : false,
-    lastIsOp : false,
-    lastIsNum : false,
-    parIsOpen : false,
-    parIsClosed : false,
 
-    plastIsMulDiv : false,
-    plastIsOp : false,
-    plastIsNum : false,
-    pparIsOpen : false,
-    pparIsClosed : false,
-
-    expIsClear : true,
-    finalExp : "",
     validate : function (exp, op) {
-        this.lastIsMulDiv = /[*/]/.test(exp[exp.length - 1]);
-        this.lastIsOp = /[*+/-]/.test(exp[exp.length - 1]);
-        this.lastIsNum = /\d/.test(exp[exp.length - 1]);
-        this.parIsOpen = /[(]/.test(exp[exp.length - 1]);
-        this.parIsClosed = /[)]/.test(exp[exp.length - 1]);
+        let finalExp = "";
+        let plastIsNum = /\d/.test(exp[exp.length - 2]);
 
-        this.plastIsMulDiv = /[*/]/.test(exp[exp.length - 2]);
-        this.plastIsOp = /[*+/-]/.test(exp[exp.length - 2]);
-        this.plastIsNum = /\d/.test(exp[exp.length - 2]);
-        this.pparIsOpen = /[(]/.test(exp[exp.length - 2]);
-        this.pparIsClosed = /[)]/.test(exp[exp.length - 2]);
-        //this.expIsClear = exp.length == 0;
+        let firstCase = /[*/)]/.test(exp);
+        let secondCase = /[*+/-][*+/-]/.test(exp);
+        let thirdCase = /([(][*/])|([(][+-][*/])/.test(exp);
+        let forthCase = /[)][0-9.]/.test(exp);
+        let fifthCase = /[(+*/-][)]/.test(exp);
+        let sixthCase = /[0-9.)][(]/.test(exp);
 
-        //In case *, / or ) is selected first when screen is clear
-        if(this.expIsClear && (this.parIsClosed || this.lastIsMulDiv)) {
+        let checkResult = thirdCase || forthCase ||
+        fifthCase || sixthCase;
+
+        //In case user selects * or / or ) first
+        if(exp.length <= 2 && firstCase && !plastIsNum) {
             let arr = exp.split("");
             arr.pop();
-            this.finalExp = arr.join("");
-            this.expIsClear = true;
-            this.parIsClosed = false;
-            this.lastIsMulDiv = false;
-            return this.finalExp;
+            finalExp = arr.join("");
+            return finalExp;
         }
 
         //In case user selects an operator twice
-        alert(exp)
-        if(this.lastIsOp && !(this.expIsClear)) {
+        if(secondCase && !thirdCase) {
             let arr = exp.split("");
             arr.pop();
             arr[arr.length - 1] = op;
-            this.finalExp = arr.join("");
-            this.lastIsOp = true;
-            return this.finalExp;
+            finalExp = arr.join("");
+            return finalExp;
         }
 
-        //In case (* or (/
-        if(this.parIsOpen && this.lastIsMulDiv) {
+        //Other safety cases
+        if(checkResult) {
             let arr = exp.split("");
             arr.pop();
-            this.finalExp = arr.join("");
-            this.parIsOpen = true;
-            this.lastIsMulDiv = false;
-            return this.finalExp;
+            finalExp = arr.join("");
+            return finalExp;
         }
 
-        //In case parenthesis is closed, accept operators only
-        if(this.parIsClosed && (this.lastIsNum || this.parIsOpen)) {
-            let arr = exp.split("");
-            arr.pop();
-            this.finalExp = arr.join("");
-            this.lastIsNum = false;
-            this.parIsOpen = false;
-            return this.finalExp;
-        }
-
-        //In case user selects ), such that () or 'op')
-        if(this.parIsClosed && (this.parIsOpen || this.lastIsOp)) {
-            let arr = exp.split("");
-            arr.pop();
-            this.finalExp = arr.join("");
-            this.parIsClosed = false;
-            return this.finalExp;
-        }
-        this.expIsClear = false;
         return exp;
+    },
+
+    finalCheck : function(exp){
+        let openPars = exp.match(/[(]/g) == null ? 0 : exp.match(/[(]/g).length;
+        let closePars = exp.match(/[)]/g) == null ? 0 : exp.match(/[)]/g).length;
+        if(openPars != closePars){
+            alert("Uneven parenthesis used!");
+            return true;
+        }
+
+        if(/[*/+-]/.test(exp[exp.length - 1])){
+            alert("Invalid end of expression!");
+            return true;
+        }
+        return false;
     }
 };
 
 function entNum(num){
     let selectedNum = num.innerHTML;
+
+    if(reset) {
+        display("");
+        expCalc = "";
+        reset = false
+    }
 
     expCalc += selectedNum;
 
@@ -97,6 +80,8 @@ function entNum(num){
 
 function entOp(op){
     let selectedOp = op.innerHTML;
+
+    reset = false;
     
     if(selectedOp === '×') selectedOp = '*';
     if(selectedOp === '÷') selectedOp = '/';
@@ -108,42 +93,31 @@ function entOp(op){
     display(expCalc);
 }
 
+function calculate(){
+    if(checker.finalCheck(expCalc) === true) return undefined;
+
+    answer = eval(expCalc);   
+    expCalc = String(answer);
+    reset = true;
+    display(String(answer));
+}
+
+function clearAll(){
+    expCalc = "";
+    answer = "";
+    reset = false;
+    display(expCalc);
+}
+
+function backSpace(){
+    let arr = expCalc.split("");
+    arr.pop();
+    expCalc = arr.join("");
+    display(expCalc);
+}
+
 function display(expression){
     let expDisp = expression.replace(/[*]/g, '×');
     expDisp = expDisp.replace(/[/]/g, '÷');
     document.getElementById('screen').innerHTML = expDisp;
 }
-
-/*function calc(opElem){  
-    opSelected = 1;
-    if(newNum == 1){ //In case user taps a sign multiple times
-        op.push(opElem.innerHTML);
-        numsStr.push(num);
-    }
-    if(opElem.innerHTML != op[0]){ // In case user changes the sign
-        op.pop(); //Dumb the last selected operation sign
-        op.push(opElem.innerHTML); //Assign the new operation sign
-    }
-    if(numsStr.length > 1){
-        if(op[0] == "+") {   
-            numsStr[0] = eval(numsStr[0]) + eval(numsStr[1]);
-            answer = numsStr[0];
-        }
-        else if(op[0] == "-"){
-            numsStr[0] = eval(numsStr[0]) - eval(numsStr[1]);
-            answer = numsStr[0];
-        }
-        else if(op[0] == "x"){
-            numsStr[0] = eval(numsStr[0]) * eval(numsStr[1]);
-            answer = numsStr[0];
-        }
-        else if(op[0] == "÷"){
-            numsStr[0] = eval(numsStr[0]) / eval(numsStr[1]);
-            answer = numsStr[0];
-        }
-        numsStr.pop();
-        op.shift();
-        display(answer);  
-    }
-    newNum = 0; 
-}*/
